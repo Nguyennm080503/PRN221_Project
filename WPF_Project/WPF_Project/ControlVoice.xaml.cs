84a85462df85a60740a6442a6c77d27bb4890a36
Duong Thi Hien <134600777+DuongHienne@users.noreply.github.com>
2024-03-25 23:16:12 +0700

    speech

--- WPF_Project/WPF_Project/ControlVoice.xaml.cs
+++ WPF_Project/WPF_Project/ControlVoice.xaml.cs
@@ -30,7 +30,7 @@ namespace WPF_Project
             Products.Add(new Product() { ProductId = 1, ProductName = "Apple", Price = 10.5m, Quantity = 100 });
             Products.Add(new Product() { ProductId = 2, ProductName = "Banana", Price = 20.75m, Quantity = 50 });
             Products.Add(new Product() { ProductId = 3, ProductName = "Coconut", Price = 15.0m, Quantity = 75 });
-
+            Products.Add(new Product() { ProductId = 4, ProductName = "Coconut", Price = 15.0m, Quantity = 75 });
         }
         #region OnpropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
@@ -46,6 +46,7 @@ namespace WPF_Project
             public string ProductName { get; set; }
             public decimal Price { get; set; }
             public int Quantity { get; set; }
+
         }
 
         private void ExportButton_Click(object sender, RoutedEventArgs e)
